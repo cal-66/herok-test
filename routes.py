0800d3f808a2023f10d78bc3d94fb25fa68fbc99
Carol Craig <carolanne.craig@gmail.com>
2017-07-16 22:05:32 +0100

    added port

--- routes.py
+++ routes.py
@@ -1,4 +1,9 @@
 #__ routes.py
+import os
+
+
+port = int(os.environ.get('PORT', 5000))
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +14,4 @@ def index():
 
 
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.run(debug = False, host='0.0.0.0', port=port)
